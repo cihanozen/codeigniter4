cbfa125d856f9549131a435468418d91ef36313d
cihanozen <cihanozen@live.com>
2023-10-01 01:13:55 +0300

    Düzenleme Yapıldı

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,29 +6,26 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Auth::index', ['filter' => 'NoLoginCheckFilter']);
+
+ $routes->get('/', 'Auth::index', ['filter' => 'NoLoginCheckFilter']);
 $routes->get('/cikis', 'Auth::logout');
 
 $routes->group('/', ['filter' => 'AuthCheckFilter'] , function($routes){
 
-    $routes->get('{locale}/dashboard', 'Dashboard::index');
     $routes->get('{locale}', 'Dashboard::index');
-    $routes->get('{locale}/user-lists', 'User::userLists');
+    $routes->get('{locale}/dashboard', 'Dashboard::index');
+   
+    $routes->group('tr', ['filter' => 'AuthCheckFilter'] , function($routes){
+
+        $routes->get('kullanici-listesi', 'User::userLists');
+    
+    });
+
+    $routes->group('en', ['filter' => 'AuthCheckFilter'] , function($routes){
+
+        $routes->get('user-lists', 'User::userLists');
+    
+    });
 
 });
 
-$routes->group('/en', ['filter' => 'AuthCheckFilter'] , function($routes){
-
-    $routes->get('{locale}/dashboard', 'Dashboard::index');
-    $routes->get('{locale}', 'Dashboard::index');
-    $routes->get('{locale}/user-lists', 'User::userLists');
-
-});
-
-$routes->group('/tr', ['filter' => 'AuthCheckFilter'] , function($routes){
-
-    $routes->get('{locale}/dashboard', 'Dashboard::index');
-    $routes->get('{locale}', 'Dashboard::index');
-    $routes->get('{locale}/kullanici-listesi', 'User::userLists');
-
-});
--- app/Views/Panel/inc/Menu.php
+++ app/Views/Panel/inc/Menu.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?php echo base_url();?>" class="brand-link">
+    <a href="<?php echo base_url($locale.'/dashboard')?>" class="brand-link">
       <img src="<?php echo base_url('assets/dist/img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">CI4 Admin</span>
     </a>
@@ -36,7 +36,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="<?php echo base_url()?>" class="nav-link">
+            <a href="<?php echo base_url($locale.'/dashboard')?>" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
